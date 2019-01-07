fd2e194d5d70f50991703d34ed7f189494ef3d1b
Nguyễn Đức Anh <30741310+rfvdqu5i@users.noreply.github.com>
2019-01-07 11:57:35 +0700

    update

--- Homework-12/Ex-02/js/script.js
+++ Homework-12/Ex-02/js/script.js
@@ -1,12 +1,12 @@
 $(function(){
     $('#open').on('click', function () {
         $('#menu').css('width','60%');
-        $(this).css('display','none');
+        $(this).children().css('display','none');
         $(this).next().children().css('display','block');
     });
     $('#close').on('click', function () {
         $('#menu').css('width','0');
-        $(this).css('display','none');
+        $(this).children().css('display','none');
         $(this).prev().children().css('display','block');
     });
     $('.support').on('click', function() {
