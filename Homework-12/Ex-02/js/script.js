75d94cc415c2f104116013b987ade34c3918a43b
Nguyễn Đức Anh <30741310+rfvdqu5i@users.noreply.github.com>
2019-01-07 11:42:10 +0700

    update

--- Homework-12/Ex-02/js/script.js
+++ Homework-12/Ex-02/js/script.js
@@ -4,13 +4,17 @@ $(function(){
         $(this).css('display','none');
         $(this).next().children().css('display','block');
     });
+    $('#menu').css('width','0');
     $('#close').on('click', function () {
         $('#menu').css('width','0');
         $(this).css('display','none');
         $(this).prev().children().css('display','block');
     });
-    $('#support').on('click', function() {
-        $('#sup').css('height','160px');
-        $('#account').css('margin-top','0');
+    $('.support').on('click', function() {
+        $('.sup').toggleClass('active');
+//        if($('.active').val() == null) {
+//            $('.account').css('margin-top','0'); 
+//            $('.account').css('transition','0.7s');
+//        } else $('.account').css('margin-top','160px');
     });
 });
