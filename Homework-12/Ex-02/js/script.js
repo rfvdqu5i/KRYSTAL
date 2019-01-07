541bc31b36f2c23d919a7fd50a9b4feb22433c39
Nguyễn Đức Anh <30741310+rfvdqu5i@users.noreply.github.com>
2019-01-07 10:17:42 +0700

    update

--- Homework-12/Ex-02/js/script.js
+++ Homework-12/Ex-02/js/script.js
@@ -1,15 +1,12 @@
 $(function(){
-   $('#addItem').on('click', function () {
-        var newTodoText = $('#newTodo').val();
-        if(newTodoText != "") {
-            $('#todos').append('<li>' + '<i class="fas fa-check check"></i>' + '<span>' + newTodoText  + '</span>' +'<i class="fas fa-times cancel"></i>' + '</li>');
-            $('#newTodo').val("");
-        } else alert("No content yet");
+    $('#open').on('click', function () {
+        $('#menu').css('width','60%');
+        $(this).css('display','none');
+        $(this).next().children().css('display','block');
     });
-    $('#todos').on('click', '.cancel', function () {
-        $(this).parent().toggleClass('done');
-        if($('.done').val() == null) {
-            $(this).prev().prev().css('opacity','0');    
-        } else $(this).prev().prev().css('opacity','1');
-    }); 
+    $('#close').on('click', function () {
+        $('#menu').css('width','0');
+        $(this).css('display','block');
+        $(this).prev().children().css('display','none');
+    });
 });
